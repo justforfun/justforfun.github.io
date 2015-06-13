349d5c39e9459a009697ee7cae6dd8d3b87808a1
Luigi Lobello <l.lobello@gmail.com>
2015-06-13 10:29:38 +0200

    First Commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AIzaSyA-oAhTzwh-31q0UcKAk66U8Y8O1vww9qg'
+};
---
+++ service-worker-new.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var REPUBBLICA_ENDPOINT = 'http://data.kataweb.it/storage/static/iwatch/iwatch-push-sent.html';
+
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+
+  var title = 'Repubblica.it';
+  var body = 'We have received a push message.';
+  var icon = '/images/icon-192x192.png';
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(
+    fetch(REPUBBLICA_ENDPOINT).then(function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        // Throw an error so the promise is rejected and catch() is executed
+        throw new Error();
+      }
+
+    // Examine the text in the response
+      return response.json().then(function(data) {
+        console.log('Data = ', JSON.stringify(data));
+        if (data.query.count === 0) {
+          // Throw an error so the promise is rejected and catch() is executed
+          throw new Error();
+        }
+
+        
+        var message = data.TESTO.text;  
+        console.log(message);
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+   }).catch(function(err) {
+      console.error('Unable to retrieve data', err);
+
+      var title = 'An error occured';
+      var message = 'We were unable to get the information for this ' +
+        'push message';
+
+      return showNotification(title, message);
+    }) 
+  );
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+
+});
