fdcfaf6d7796c59db9ae3c8c7a198f302ac02e44
Luigi Lobello <l.lobello@gmail.com>
2015-06-21 19:53:34 +0200

    lo dei parametri di inizializzazione Parse

--- yahoo-service-worker.js
+++ yahoo-service-worker.js
@@ -57,6 +57,8 @@ function showNotification(title, body, icon, data) {
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
   console.log('PARSE_APPLICATION_ID:' + PARSE_APPLICATION_ID);
+  console.log('PARSE_JAVASCRIPT_KEY:' + PARSE_JAVASCRIPT_KEY);
+
   if (!Parse) {
     Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY, PARSE_MASTER_KEY);
   };
