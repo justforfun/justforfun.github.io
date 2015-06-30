3167295053cde368e63c85ae3df08c3eeea84d8e
Luigi Lobello <l.lobello@gmail.com>
2015-06-30 17:28:00 +0200

    configurata api key e sender id di prod

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyA-oAhTzwh-31q0UcKAk66U8Y8O1vww9qg'
+  gcmAPIKey: 'AIzaSyBViWF9gHrx55nu-HGytkrlYPW61q6iNhI'
 };
