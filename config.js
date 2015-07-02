f53b01a1bcd8ffbb4973dcd1fb8e93626438c434
Luigi Lobello <l.lobello@gmail.com>
2015-07-02 10:21:44 +0200

    ambiente di prod

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
   // prod gcmAPIKey: 'AIzaSyBViWF9gHrx55nu-HGytkrlYPW61q6iNhI'
-<<<<<<< HEAD
+  // test gcmAPIKey: 'AIzaSyA00CL5HW81bQqjAdyyPnuCMlJq5KlX0MA'
   gcmAPIKey: 'AIzaSyBViWF9gHrx55nu-HGytkrlYPW61q6iNhI'
-=======
-  gcmAPIKey: 'AIzaSyA00CL5HW81bQqjAdyyPnuCMlJq5KlX0MA'
->>>>>>> FETCH_HEAD
 };
