d60dea60a7a716104cd652cfd4a35043754d0921
Luigi Lobello <l.lobello@gmail.com>
2016-05-25 08:39:33 +0200

    aggiornato embed.js ricevuto da scribble

--- test-perf/embed.js
+++ test-perf/embed.js
@@ -1,143 +1,204 @@
 /*! custom-embed - v0.0.8 */
-!
-function (a, b, c) {
-    function d() {
-        var b = "undefined" != typeof a.jQuery;
-        if (b || g === h) {
-            if (b) e(a, a.jQuery);
-            else if (g === h) throw new Error("The Scribblelive embed requires jQuery");
-            clearInterval(f)
+(function(window, $, undefined) {
+    if (typeof $ === "undefined") {
+        var intervalWaitjQuery = setInterval(waitjQuery, 100), tries = 0, maxTries = 100;
+        function waitjQuery() {
+            var jQueryFound = typeof window.jQuery !== "undefined";
+            if (jQueryFound || tries === maxTries) {
+                if (jQueryFound) {
+                    jQueryLoaded(window, window.jQuery);
+                } else if (tries === maxTries) {
+                    throw new Error("The Scribblelive embed requires jQuery");
+                }
+                clearInterval(intervalWaitjQuery);
+            }
+            tries++;
         }
-        g++
+    } else {
+        jQueryLoaded(window, window.jQuery);
     }
-    function e(a, b, c) {
-        function d() {
-            //var a = ["https://10.20.20.10/", "http://10.20.20.10/", "https://s3.amazonaws.com/customerfiles.scribblelive.com/", "http://s3.amazonaws.com/customerfiles.scribblelive.com/", "http://customerfiles.scribblelive.com.s3.amazonaws.com/"];
-            var a = ["https://10.20.20.10/", "http://10.20.20.10/", "https://www.repstatic.it/customerfiles.scribblelive.com/", "http://www.repstatic.it/customerfiles.scribblelive.com/", "http://customerfiles.scribblelive.com.s3.amazonaws.com/"];
-            new RegExp(a.join("|")), new Error;
-            return a[2]
+    function jQueryLoaded(window, $, undefined) {
+        function getScriptDomain() {
+            var whitelist = [ "https://10.20.20.10/", "http://10.20.20.10/","https://www.repstatic.it/customerfiles.scribblelive.com/groupo_espresso/", "https://s3.amazonaws.com/customerfiles.scribblelive.com/", "http://s3.amazonaws.com/customerfiles.scribblelive.com/", "http://customerfiles.scribblelive.com.s3.amazonaws.com/" ], reWhiteList = new RegExp(whitelist.join("|")), err = new Error(), match;
+            return whitelist[2];
         }
-        function e(a, b, d) {
-            var e, f, g = d === c ? 2166136261 : d;
-            for (e = 0, f = a.length; f > e; e++) g ^= a.charCodeAt(e), g += (g << 1) + (g << 4) + (g << 7) + (g << 8) + (g << 24);
-            return b ? ("0000000" + (g >>> 0).toString(16)).substr(-8) : g >>> 0
+        var root = getScriptDomain.call();
+        var modules = {
+            "key-event": {
+                css_required: [ root + "css/key_events.css" ],
+                js_required: [ {
+                    required: null,
+                    toLoad: [ root + "js/key_events.js" ]
+                } ],
+                init: function() {
+                    custom_key_event.init(this);
+                    $("head").append('<script src="' + root + 'implementation/custom-embed/latest/js/embed.min.js">');
+                }
+            }
+        };
+        function hashFnv32a(str, asString, seed) {
+            var i, l, hval = seed === undefined ? 2166136261 : seed;
+            for (i = 0, l = str.length; i < l; i++) {
+                hval ^= str.charCodeAt(i);
+                hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+            }
+            if (asString) {
+                return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+            }
+            return hval >>> 0;
         }
-        function f(a, b, c) {
-            return c.indexOf(a) === b
+        function array_unique(value, index, self) {
+            return self.indexOf(value) === index;
         }
-        function g(a) {
-            this.toLoad = b.map(a, function (a) {
-                return b.ajax({
+        var rand = new Date().getTime();
+        function LoadRequiredStylesheets(required) {
+            this.toLoad = $.map(required, function(url) {
+                return $.ajax({
                     method: "GET",
-                    url: a + "?" + k,
-                    crossDomain: !0
-                })
-            })
-        }
-        function h(c) {
-            c = b.map(c, function (c) {
-                return null != c.required && (c.required = b.map(c.required, function (b) {
-                    return b.match(/^\/\//) ? a.location.protocol + b : b
-                })), c.toLoad = b.map(c.toLoad, function (b) {
-                    return b.match(/^\/\//) ? a.location.protocol + b : b
-                }), c
+                    url: url + "?" + rand,
+                    crossDomain: true
+                });
             });
-            var d = b.map(c, function (a) {
-                return a.toLoad
-            }),
-                e = d.filter(f);
-            this.required_with_dependency = c, this.required = e, this.deferreds = [], this.loaded = [], this.pending = [], this.loadingFinished = new b.Deferred
         }
-        var i = d.call(),
-            j = {
-                "key-event": {
-                    css_required: [i + "groupo_espresso/css/key_events.css"],
-                    js_required: [{
-                        required: null,
-                        toLoad: [i + "groupo_espresso/js/key_events.js"]
-                    }],
-                    init: function () {
-                        //custom_key_event.init(this), b("head").append('<script src="' + i + 'implementation/custom-embed/latest/js/embed.min.js">')
-                        custom_key_event.init(this), b("head").append('<script src="' + i + 'groupo_espresso/custom-embed/latest/js/embed.min.js">')
+        LoadRequiredStylesheets.prototype.loading = function() {
+            var dfd = $.when.apply($, this.toLoad);
+            dfd.done(function() {
+                var list = [];
+                if (typeof arguments[1] === "string") {
+                    list.push(arguments);
+                } else {
+                    list = arguments;
+                }
+                var i, len, dfd;
+                for (i = 0, len = list.length; i < len; i++) {
+                    dfd = list[i][2];
+                    dfd.done(function() {
+                        var key = hashFnv32a(this.url, true);
+                        if ($("#" + key).length == 0) {
+                            var stylesheet = $("<link>", {
+                                rel: "stylesheet",
+                                id: key,
+                                "class": "groupo_espresso",
+                                type: "text/css",
+                                href: this.url
+                            });
+                            if ($('link[rel="stylesheet"].groupo_espresso').length) {
+                                stylesheet.insertAfter('link[rel="stylesheet"].groupo_espresso:last');
+                            } else if ($('link[rel="stylesheet"]:not(.groupo_espresso), style').length) {
+                                stylesheet.insertBefore($('link[rel="stylesheet"]:first, style:first').first());
+                            } else {
+                                stylesheet.appendTo("head");
+                            }
+                        }
+                    });
+                }
+            }).fail(function(jqXHR) {
+                console.warn(new Error("Loading " + this.url + " failed [" + jqXHR.status + "] " + jqXHR.statusText));
+            });
+            return dfd;
+        };
+        function LoadRequiredJavascript(required_with_dependency) {
+            required_with_dependency = $.map(required_with_dependency, function(el) {
+                if (el.required != null) {
+                    el.required = $.map(el.required, function(url) {
+                        if (url.match(/^\/\//)) return window.location.protocol + url;
+                        return url;
+                    });
+                }
+                el.toLoad = $.map(el.toLoad, function(url) {
+                    if (url.match(/^\/\//)) return window.location.protocol + url;
+                    return url;
+                });
+                return el;
+            });
+            var list_required = $.map(required_with_dependency, function(el) {
+                return el.toLoad;
+            });
+            var unique_required = list_required.filter(array_unique);
+            this.required_with_dependency = required_with_dependency;
+            this.required = unique_required;
+            this.deferreds = [];
+            this.loaded = [];
+            this.pending = [];
+            this.loadingFinished = new $.Deferred();
+        }
+        LoadRequiredJavascript.prototype.loading = function() {
+            this._checkRequirements(null);
+            return this.loadingFinished.promise();
+        };
+        LoadRequiredJavascript.prototype._checkRequirements = function(loaded) {
+            var _self = this, i, len, url;
+            var toLoad = $.map(_self.required_with_dependency, function(element, index) {
+                if (loaded == null && element.required == null) return element.toLoad;
+                if (element.required != null) {
+                    if (0 <= element.required.indexOf(loaded) && element.required.length == 1) {
+                        return element.toLoad;
+                    } else if (0 <= element.required.indexOf(loaded) && element.required.length > 1) {
+                        is_loaded = $.map(element.required, function(url) {
+                            if (0 <= _self.loaded.indexOf(url)) return url;
+                            return;
+                        });
+                        if (is_loaded.length == element.required.length) return element.toLoad;
                     }
                 }
-            },
-            k = (new Date).getTime();
-        g.prototype.loading = function () {
-            var a = b.when.apply(b, this.toLoad);
-            return a.done(function () {
-                var a = [];
-                "string" == typeof arguments[1] ? a.push(arguments) : a = arguments;
-                var c, d, f;
-                for (c = 0, d = a.length; d > c; c++) f = a[c][2], f.done(function () {
-                    var a = e(this.url, !0);
-                    if (0 == b("#" + a).length) {
-                        var c = b("<link>", {
-                            rel: "stylesheet",
-                            id: a,
-                            "class": "groupo_espresso",
-                            type: "text/css",
-                            href: this.url
-                        });
-                        b('link[rel="stylesheet"].groupo_espresso').length ? c.insertAfter('link[rel="stylesheet"].groupo_espresso:last') : b('link[rel="stylesheet"]:not(.groupo_espresso), style').length ? c.insertBefore(b('link[rel="stylesheet"]:first, style:first').first()) : c.appendTo("head")
-                    }
-                })
-            }).fail(function (a) {
-                console.warn(new Error("Loading " + this.url + " failed [" + a.status + "] " + a.statusText))
-            }), a
-        }, h.prototype.loading = function () {
-            return this._checkRequirements(null), this.loadingFinished.promise()
-        }, h.prototype._checkRequirements = function (a) {
-            var c, d, e, g = this,
-                h = b.map(g.required_with_dependency, function (c, d) {
-                    if (null == a && null == c.required) return c.toLoad;
-                    if (null != c.required) {
-                        if (0 <= c.required.indexOf(a) && 1 == c.required.length) return c.toLoad;
-                        if (0 <= c.required.indexOf(a) && c.required.length > 1 && (is_loaded = b.map(c.required, function (a) {
-                            return 0 <= g.loaded.indexOf(a) ? a : void 0
-                        }), is_loaded.length == c.required.length)) return c.toLoad
-                    }
-                });
-            for (h = h.filter(f), c = h.length - 1; c >= 0; c--) e = h[c], (0 <= g.loaded.indexOf(e) || 0 <= g.pending.indexOf(e)) && h.splice(c, 1);
-            for (c = 0, d = h.length; d > c; c++) e = h[c], g._getRequiredScript.call(g, e);
-            g.loaded.length == g.required.length && g.loadingFinished.resolveWith(b, this.deferreds)
-        }, h.prototype._getRequiredScript = function (a) {
-            var c = this;
-            c.pending.push(a);
-            var d = b.ajax({
-                dataType: "script",
-                cache: !0,
-                url: a
-            }).done(function () {
-                c.loaded.push(this.url), c._checkRequirements(this.url)
-            }).fail(function (a) {
-                console.warn(new Error("Loading " + this.url + " failed [" + a.status + "] " + a.statusText)), c.loadingFinished.rejectWith(b, this.deferreds)
             });
-            c.deferreds.push(d)
-        }, b(function () {
-            var a = [],
-                c = [],
-                d = [];
-            b(".scrbbl-custom-embed").each(function () {
-                var e, f = b(this),
-                    g = f.attr("data-src").split("/");
-                e = j[g[0]], "undefined" != typeof e && (d.push({
-                    src: g[0],
-                    element: f
-                }), a = b.merge(a, e.css_required), c = b.merge(c, e.js_required))
-            }), a = a.filter(f), c = c.filter(f);
-            var e = new g(a),
-                i = new h(c);
-            b.when(e.loading(), i.loading()).done(function () {
-                var a, b, c;
-                for (a = 0, b = d.length; b > a; a++) c = d[a], j[c.src].init.call(c.element)
-            }).fail(function () {
-                throw new Error("Failed to load all required resources")
-            })
-        })
+            toLoad = toLoad.filter(array_unique);
+            for (i = toLoad.length - 1; i >= 0; i--) {
+                url = toLoad[i];
+                if (0 <= _self.loaded.indexOf(url) || 0 <= _self.pending.indexOf(url)) {
+                    toLoad.splice(i, 1);
+                }
+            }
+            for (i = 0, len = toLoad.length; i < len; i++) {
+                url = toLoad[i];
+                _self._getRequiredScript.call(_self, url);
+            }
+            if (_self.loaded.length == _self.required.length) {
+                _self.loadingFinished.resolveWith($, this.deferreds);
+            }
+        };
+        LoadRequiredJavascript.prototype._getRequiredScript = function(url) {
+            var _self = this;
+            _self.pending.push(url);
+            var dfd = $.ajax({
+                dataType: "script",
+                cache: true,
+                url: url
+            }).done(function() {
+                _self.loaded.push(this.url);
+                _self._checkRequirements(this.url);
+            }).fail(function(jqXHR) {
+                console.warn(new Error("Loading " + this.url + " failed [" + jqXHR.status + "] " + jqXHR.statusText));
+                _self.loadingFinished.rejectWith($, this.deferreds);
+            });
+            _self.deferreds.push(dfd);
+        };
+        $(function() {
+            var css_required = [], js_required = [], modules_to_load = [];
+            $(".scrbbl-custom-embed").each(function() {
+                var $this = $(this), source = $this.attr("data-src").split("/"), module, i, l, src;
+                if (module = modules[source[0]], "undefined" !== typeof module) {
+                    modules_to_load.push({
+                        src: source[0],
+                        element: $this
+                    });
+                    css_required = $.merge(css_required, module.css_required);
+                    js_required = $.merge(js_required, module.js_required);
+                }
+            });
+            css_required = css_required.filter(array_unique);
+            js_required = js_required.filter(array_unique);
+            var lrs = new LoadRequiredStylesheets(css_required);
+            var lrj = new LoadRequiredJavascript(js_required);
+            $.when(lrs.loading(), lrj.loading()).done(function() {
+                var i, len, m;
+                for (i = 0, len = modules_to_load.length; i < len; i++) {
+                    m = modules_to_load[i];
+                    modules[m.src].init.call(m.element);
+                }
+            }).fail(function() {
+                throw new Error("Failed to load all required resources");
+            });
+        });
     }
-    if ("undefined" == typeof b) var f = setInterval(d, 100),
-        g = 0,
-        h = 100;
-    else e(a, a.jQuery)
-}(window, window.jQuery);
+})(window, window.jQuery);
