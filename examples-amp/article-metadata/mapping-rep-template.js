0f5287ea78ace995fc10d8620f2f0577f15ff950
Luigi Lobello <l.lobello@gmail.com>
2017-02-19 13:57:07 +0100

    test

--- examples-amp/article-metadata/mapping-rep-template.js
+++ examples-amp/article-metadata/mapping-rep-template.js
@@ -1,3 +1,8 @@
 {
-"items" : $input.json('$..entries[?(@.categories && !@.relateds)]')
-}
+"items" : [{
+    "displayName": "Articoli correlati",
+    "showContent": true,
+    "content":$input.json('$..entries[?(@.categories && !@.relateds)]')
+}]
+}    
+    
