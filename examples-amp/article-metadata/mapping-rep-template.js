2c9c6d70352f333ec3a9b954a2b10ee7a91e617c
Luigi Lobello <l.lobello@gmail.com>
2017-02-22 19:52:41 +0100

    test

--- examples-amp/article-metadata/mapping-rep-template.js
+++ examples-amp/article-metadata/mapping-rep-template.js
@@ -1,9 +1,15 @@
+##"content":$input.json('$..entries[?(@.categories && !@.relateds)]')
 #set($allEntries = $input.json('$..entries[?(@.categories && !@.relateds)]'))
 {
 "items" : [{
     "displayName": "Dalla Home Page",
     "showContent": true,
     "showWebPublicationDate": true,
-    "content":$input.json('$..entries[?(@.categories && !@.relateds)]')
+    "content":{
+#foreach($key in $allEntries.keySet())
+"$key" : "$util.escapeJavaScript($allEntries.get($key))"
+    #if($velocityCount>9),#end
+#end
+}
 }]
 }        
